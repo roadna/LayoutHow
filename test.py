62862ae1ff1c5474623f1952668dace2f77b38c5
roadna <me@roadna.com>
2016-11-07 23:15:24 +0800

    grades and middle usage testing

--- test.py
+++ test.py
@@ -4,29 +4,25 @@ import LayoutAnalysis
 import Layouts
 
 la = LayoutAnalysis.Analyze(sys.argv[1])
-print(Layouts.qwerty)
-
-heat_map = la.heat_map(Layouts.qwerty)
-print(heat_map)
 
 #finger_usage = la.finger_usage(Layouts.qwerty, heat_map)
 #print(finger_usage)
+heat_map = la.heat_map(Layouts.qwerty)
 grades = la.grade(Layouts.qwerty, heat_map)
-print(grades[1])
 middle_usage = la.middle_usage(Layouts.qwerty, heat_map)
-print(middle_usage)
+print("qwerty", grades[1], middle_usage)
 
-grades = la.grade(Layouts.dvorak)
-print(grades[1])
-middle_usage = la.middle_usage(Layouts.dvorak)
-print(middle_usage)
+heat_map = la.heat_map(Layouts.dvorak)
+grades = la.grade(Layouts.dvorak, heat_map)
+middle_usage = la.middle_usage(Layouts.dvorak, heat_map)
+print("dvorak", grades[1], middle_usage)
 
-grades = la.grade(Layouts.colemak)
-print(grades[1])
-middle_usage = la.middle_usage(Layouts.colemak)
-print(middle_usage)
+heat_map = la.heat_map(Layouts.colemak)
+grades = la.grade(Layouts.colemak, heat_map)
+middle_usage = la.middle_usage(Layouts.colemak, heat_map)
+print("colemak", grades[1], middle_usage)
 
-grades = la.grade(Layouts.norman)
-print(grades[1])
-middle_usage = la.middle_usage(Layouts.norman)
-print(middle_usage)
+heat_map = la.heat_map(Layouts.norman)
+grades = la.grade(Layouts.norman, heat_map)
+middle_usage = la.middle_usage(Layouts.norman, heat_map)
+print("norman", grades[1], middle_usage)
